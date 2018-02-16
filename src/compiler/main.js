fdffc35c665ce1032b4924e9811ffec7955188ee
cellis <cameronellis.ellis@gmail.com>
2018-02-16 14:36:13 -0800

    Add watching back in

--- src/compiler/main.js
+++ src/compiler/main.js
@@ -12,7 +12,43 @@ const {
   ConsoleReporter,
   Runner: CodegenRunner,
   FileIRParser: RelayJSModuleParser,
+  WatchmanClient,
+  DotGraphQLParser,
 } = RelayCompiler;
+
+function buildWatchExpression(options: {
+  extensions: Array<string>,
+  include: Array<string>,
+  exclude: Array<string>,
+}) {
+  return [
+    'allof',
+    ['type', 'f'],
+    ['anyof', ...options.extensions.map(ext => ['suffix', ext])],
+    [
+      'anyof',
+      ...options.include.map(include => ['match', include, 'wholename']),
+    ],
+    ...options.exclude.map(exclude => ['not', ['match', exclude, 'wholename']]),
+  ];
+}
+
+// Ensure that a watchman "root" file exists in the given directory
+// or a parent so that it can be watched
+const WATCHMAN_ROOT_FILES = ['.git', '.hg', '.watchmanconfig'];
+function hasWatchmanRootFile(testPath) {
+  while (path.dirname(testPath) !== testPath) {
+    if (
+      WATCHMAN_ROOT_FILES.some(file => {
+        return fs.existsSync(path.join(testPath, file));
+      })
+    ) {
+      return true;
+    }
+    testPath = path.dirname(testPath);
+  }
+  return false;
+}
 
 /*
 * Most of the code in this run method are ripped from:
@@ -22,7 +58,7 @@ const run = async (options: {
   schema: string,
   src: string,
   webpackConfig: string,
-  extensions:  Array<string>
+  extensions: Array<string>,
   watch?: ?boolean,
 }) => {
   const srcDir = path.resolve(process.cwd(), options.src);
@@ -42,28 +78,57 @@ const run = async (options: {
     schemaPath = await graphqlJSCompiler(schemaPath, srcDir, customWebpackConfig);
   }
 
+  if (options.watch && !options.watchman) {
+    throw new Error('Watchman is required to watch for changes.');
+  }
+
+  if (options.watch && !hasWatchmanRootFile(srcDir)) {
+    throw new Error(`
+--watch requires that the src directory have a valid watchman "root" file.
+
+Root files can include:
+- A .git/ Git folder
+- A .hg/ Mercurial folder
+- A .watchmanconfig file
+
+Ensure that one such file exists in ${srcDir} or its parents.
+    `.trim(),
+    );
+  }
+
+  const useWatchman = options.watchman && (await WatchmanClient.isAvailable());
+
   console.log(`schemaPath: ${schemaPath}`);
   clean(srcDir);
 
   const reporter = new ConsoleReporter({verbose: true});
   const parserConfigs = {
-    default: {
+    js: {
       baseDir: srcDir,
       getFileFilter: RelayJSModuleParser.getFileFilter,
       getParser: RelayJSModuleParser.getParser,
       getSchema: () => getSchema(schemaPath),
-      filepaths: getFilepathsFromGlob(srcDir, {
-        extensions: options.extensions,
-        include: ['**'],
-        exclude: [
-          '**/node_modules/**',
-          '**/__mocks__/**',
-          '**/__tests__/**',
-          '**/__generated__/**',
-        ],
-      }),
+      watchmanExpression: useWatchman ? buildWatchExpression(options) : null,
+      filepaths: useWatchman ? null : getFilepathsFromGlob(srcDir, options),
+    },
+    graphql: {
+      baseDir: srcDir,
+      getParser: DotGraphQLParser.getParser,
+      getSchema: () => getSchema(schemaPath),
+      watchmanExpression: useWatchman ? buildWatchExpression({
+        extensions: ['graphql'],
+        include: options.include,
+        exclude: options.exclude,
+      }) : null,
+      filepaths: useWatchman ? null :
+        getFilepathsFromGlob(srcDir, {
+          extensions: ['graphql'],
+          include: options.include,
+          exclude: options.exclude,
+        }),
     },
   };
+
   const writerConfigs = {
     default: {
       getWriter: getRelayFileWriter(srcDir, persistQuery),
@@ -82,7 +147,7 @@ const run = async (options: {
   let result = '';
   try {
     // the real work is done here
-    result = options.watch ? await codegenRunner.compileAll() : await codegenRunner.watchAll();
+    result = options.watch ? await codegenRunner.watchAll() : await codegenRunner.compileAll();
   } catch (err) {
     console.log(`Error codegenRunner.compileAll(): ${err}`);
     throw err;
@@ -108,6 +173,23 @@ const argv = yargs // eslint-disable-line prefer-destructuring
     schema: {
       describe: 'Path to schema.js or schema.graphql or schema.json',
       demandOption: false,
+      type: 'string',
+    },
+    include: {
+      array: true,
+      default: ['**'],
+      describe: 'Directories to include under src',
+      type: 'string',
+    },
+    exclude: {
+      array: true,
+      default: [
+        '**/node_modules/**',
+        '**/__mocks__/**',
+        '**/__tests__/**',
+        '**/__generated__/**',
+      ],
+      describe: 'Directories to ignore under src',
       type: 'string',
     },
     src: {
